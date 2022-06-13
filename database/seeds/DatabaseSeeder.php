c777ecc62dea0c987d269bca1bc44ed3f0fb0d90
Luis Timaná <60704967+luistimana@users.noreply.github.com>
2022-06-13 11:42:21 -0500

    Authentication, Database Seeding
    Authentication, Database Seeding

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'Admin Istrador',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('password'),
+        ]);
     }
 }
